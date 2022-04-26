e6fbf8d8b20072a921ca7ac83e2dcd693b7d8028
Eugene Menski <eugene.menski@gmail.com>
2022-04-26 14:09:24 +0300

    Print even numbers within [1, N] range

---
+++ task8/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.*/
+
+Console.WriteLine("Enter a number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write("{0} ", Convert.ToString(i));
+        }
+    }
+}
+else
+{
+    Console.Write("Incorrect number.");
+}
