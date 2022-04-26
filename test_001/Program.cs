9a96dc91ba437c567faff11b4502431440a99f34
Eugene Menski <eugene.menski@gmail.com>
2022-04-26 19:20:43 +0300

    first commit to the test_001 app

---
+++ test_001/Program.cs
@@ -0,0 +1,22 @@
+﻿//directive. Says to the compiler that the program uses the System namespace.
+//Namespace is a grouping of related code elements: classes, structures, enums, etc.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+
+    //A Simple Program to display the words Hello World
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
