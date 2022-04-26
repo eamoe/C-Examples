b5f105e4d8bf75f34f127156b1d2c57903ec6918
Eugene Menski <eugene.menski@gmail.com>
2022-04-26 12:27:35 +0300

    Find which number of 2 is bigger

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше. */
+
+Console.WriteLine("Enter first number");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("{0} bigger than {1}", Convert.ToString(firstNumber), Convert.ToString(secondNumber));
+}
+else
+{
+    Console.WriteLine("{0} bigger than {1}", Convert.ToString(secondNumber),Convert.ToString(firstNumber));
+}
