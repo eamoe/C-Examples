ed10bc0e8071df81cdfacc31c1ae7ab1234a319a
Eugene Menski <eugene.menski@gmail.com>
2022-04-26 13:56:42 +0300

    Check whether a number is odd or even

---
+++ task6/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).*/
+
+Console.WriteLine("Enter a number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number % 2)
+{
+    case 0:
+        Console.WriteLine("Number {0} is even", Convert.ToString(number));
+        break;
+    default:
+        Console.WriteLine("Number {0} is odd", Convert.ToString(number));
+        break;
+}
