dd30350c1e6c6178adcc1358b1db4f79c259a42a
Eugene Menski <eugene.menski@gmail.com>
2022-05-13 11:24:13 +0300

    Find the biggest digit of a random number

---
+++ task9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру.
+
+int randomNumber = new Random().Next(10, 100);
+
+int numberOne = randomNumber % 10;
+int numberTwo = randomNumber / 10;
+
+Console.Write($"У числа {randomNumber} большая цифра ");
+
+if (numberOne > numberTwo)
+{
+    Console.Write(numberOne);
+}
+else
+{
+    Console.Write(numberTwo);
+}
