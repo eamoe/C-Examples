b8633dde17f54295ba9778b2b2641da7166c7a20
Eugene Menski <eugene.menski@gmail.com>
2022-05-13 14:12:36 +0300

    Finding third digit of a number

---
+++ task13/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+//int randomNumber = new Random().Next(1, 100000);
+
+int randomNumber = -14172;
+
+//Adopting negative numbers
+int positiveRandomNumber = randomNumber;
+if (positiveRandomNumber < 0) positiveRandomNumber *= -1;
+
+//Count digits in a number
+string numberStr = Convert.ToString(positiveRandomNumber);
+
+if (numberStr.Length < 3)
+{
+    System.Console.Write($"В числе {randomNumber} нет третьей цифры");
+}
+else
+{
+    int thirdDigit = Convert.ToInt32(numberStr.Substring(2, 1));
+    
+    System.Console.Write($"В числе {randomNumber} третья цифра равна {thirdDigit}");
+}
