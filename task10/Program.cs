e18c1ffc2dbb854e31dfe38170b6db3bc1e8901b
Eugene Menski <eugene.menski@gmail.com>
2022-05-13 13:04:03 +0300

    Finding the second digit of a number

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int randomNumber = new Random().Next(100, 1000);
+
+int secondDigit = (randomNumber % 100) / 10;
+
+System.Console.Write($"Вторая цифра числа {randomNumber} равна {secondDigit}");
