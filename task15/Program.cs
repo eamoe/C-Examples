a83a23f5f2702f3690f4429e50f0acc4b3a4ff38
Eugene Menski <eugene.menski@gmail.com>
2022-05-13 14:30:53 +0300

    Match a number with days of the week

---
+++ task15/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 && number > 7)
+{
+    Console.WriteLine($"Число {number} не соответствует ни одному дню недели!");
+}
+else
+{
+    switch (number)
+    {
+        case 1:
+            Console.WriteLine("Нет, это не выходной!");
+            break;
+
+        case 2:
+            Console.WriteLine("Нет, это не выходной!");
+            break;
+
+        case 3:
+            Console.WriteLine("Нет, это не выходной!");
+            break;
+
+        case 4:
+            Console.WriteLine("Нет, это не выходной!");
+            break;
+
+        case 5:
+            Console.WriteLine("Нет, это не выходной!");
+            break;
+
+        case 6:
+            Console.WriteLine("Да, это выходной!");
+            break;
+
+        case 7:
+            Console.WriteLine("Да, это выходной!");
+            break;
+
+        default:
+            Console.WriteLine($"Число {number} не соответствует ни одному дню недели!");
+            break;
+    }
+}
