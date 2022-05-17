ae33c6bcd307e2035ff61aeaeb431ac20353040c
Eugene Menski <eugene.menski@gmail.com>
2022-05-17 17:47:30 +0300

    Cubes of number from 1 to N

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+void cubes(int n)
+{
+    Console.Write($"Кубы чисел от 1 до {n} равны: ");
+
+    for(int i = 1; i <= n; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
+
+Console.Write("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+cubes(n);
