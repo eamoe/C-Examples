597651cc244e656e0c80de5acc76f0c1157e5300
Eugene Menski <eugene.menski@gmail.com>
2022-05-17 16:55:31 +0300

    Check if number is a palindrome

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да */
+
+Boolean isPalindrome(int number)
+{
+    string numberStr = Convert.ToString(number);
+    
+    Boolean check = true;
+
+    for(int i = 0; i < (numberStr.Length - i -1); i++)
+    {
+        if (numberStr[i] != numberStr[numberStr.Length - i -1])
+        {
+            check = false;
+        }
+    }
+
+    return check;    
+}
+
+Console.WriteLine("Введите целое пятизначное число: ");
+
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (isPalindrome(userNumber))
+{
+    Console.WriteLine($"Число {userNumber} является палиндромом!");
+}
+else
+{
+    Console.WriteLine($"Число {userNumber} НЕ является палиндромом!");
+}
