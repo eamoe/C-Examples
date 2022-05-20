5545d01a2b2d47f45cd5f5598dd67a479f0ed121
Eugene Menski <eugene.menski@gmail.com>
2022-05-20 12:05:42 +0300

    Calculate digits sum of an entered number

--- task27/Program.cs
+++ task27/Program.cs
@@ -3,3 +3,26 @@
 82 -> 10
 9012 -> 12 */
 
+int sumOfDigits(int number)
+{
+    if (number < 0) number = number * (-1);
+    
+    string numberStr = Convert.ToString(number);
+    
+    int sum = 0;
+
+    for(int i = 0; i < numberStr.Length; i++)
+    {
+        int currentNumber = Convert.ToInt32(numberStr.Substring(i, 1));
+        
+        sum = sum + currentNumber;
+    }
+
+    return sum;    
+}
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр числа {number} равна: {sumOfDigits(number)}");
