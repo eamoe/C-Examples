c65a2f27ff4f4bf41815518ab0a78c153b255235
Eugene Menski <eugene.menski@gmail.com>
2022-05-20 14:35:05 +0300

    Generate an array of 8 elements

--- task29/Program.cs
+++ task29/Program.cs
@@ -2,3 +2,28 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33] */
 
+void printArray(int [] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("{0} ", arr[i]);
+    }
+}
+
+int [] generateArray(int size)
+{
+    int [] arr = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+
+    return arr;
+}
+
+int [] array = generateArray(8);
+
+Console.Write("Сгенерированный массив: ");
+
+printArray(array);
