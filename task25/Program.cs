391b4c425edbd6675bd92f723f37f48c4c94b0fc
Eugene Menski <eugene.menski@gmail.com>
2022-05-20 14:45:47 +0300

    Get a pow b

--- task25/Program.cs
+++ task25/Program.cs
@@ -2,3 +2,24 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 
+int aPowerB(int a, int b)
+{
+    int result = 1;
+    
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+
+    return result;
+}
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите степень, в которую хотите возвести число {number}: ");
+
+int power = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Результат возведения числа {number} в степень {power} равен: {aPowerB(number, power)}");
