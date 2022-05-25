63e8e947e028e48c4d0042342b94c29eef05edb5
Eugene Menski <eugene.menski@gmail.com>
2022-05-25 12:14:51 +0300

    multiply pairs

--- task37/Program.cs
+++ task37/Program.cs
@@ -2,3 +2,64 @@
 [1 2 3 4 5] -> 5 8 3
 [6 7 3 6] -> 36 21 */
 
+int [] sumPairs(int [] arr)
+{
+    int size;
+    
+    if(arr.Length % 2 == 0)
+    {
+        size = arr.Length / 2;
+    }
+    else
+    {
+        size = (arr.Length / 2) + 1;
+    }
+    
+    int [] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        if(size % 2 == 0)
+        {
+            result[i] = arr[i] * arr[arr.Length - i - 1];
+        }
+        else
+        {
+            result[i] = arr[i];
+        }
+    }
+
+    return result;    
+}
+
+int [] generateArray(int size, int from, int to)
+{
+    int [] arr = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+
+    return arr;
+}
+
+void printArray(int [] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("{0} ", arr[i]);
+    }
+}
+
+int [] array = generateArray(7, 0, 10);
+
+Console.Write("Сгенерированный массив: ");
+
+printArray(array);
+
+int [] summedArray = sumPairs(array);
+
+Console.Write("\nПросуммированный массив: ");
+
+printArray(summedArray);
