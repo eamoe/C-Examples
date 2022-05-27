586412b731b9eb340897a39fdd6cb532e524e52c
Eugene Menski <eugene.menski@gmail.com>
2022-05-27 20:10:04 +0300

    Count positive number

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+
+//Get string of numbers
+string getStringOfNumbers()
+{
+    Console.Write("Введите M чисел через пробел или запятую и пробел: ");
+
+    string str = Console.ReadLine();
+    
+    return str;
+}
+
+//Convert string of numbers to array of numbers
+int [] convertToNumbersArray(string str)
+{
+    string [] separator = {" ", ", "};
+    string [] numbersStr = str.Split(separator, StringSplitOptions.None);
+
+    int [] numbers = new int[numbersStr.Length];
+
+    for (int i = 0; i < numbersStr.Length; i++)
+    {
+        numbers[i] = Convert.ToInt32(numbersStr[i]);
+    }
+
+    return numbers;
+}
+
+//Count positive numbers
+int countPositiveNumbers(int [] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+
+    return count;
+}
+
+string numbersStr = getStringOfNumbers();
+
+int [] numbers = convertToNumbersArray(numbersStr);
+
+Console.Write($"Положительных чисел больше нуля среди введенных: {countPositiveNumbers(numbers)}");
