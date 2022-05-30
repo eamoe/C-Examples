7e36d06019d1db203b9d0033fae194fb5b3c2392
Eugene Menski <eugene.menski@gmail.com>
2022-05-30 20:14:23 +0300

    Simplified if statement

--- task38/Program.cs
+++ task38/Program.cs
@@ -21,15 +21,17 @@ void printArray(double [] arr)
     }
 }
 
-double findMinMax(double [] array, Boolean _isMax)
+double findMinMax(double [] array, Boolean isMax)
 {    
     double result = array[0];
 
     for (int i = 0; i < array.Length; i++)
     {
-        if (result < array[i] && _isMax == true) result = array[i];
-
-        if (result > array[i] && _isMax == false) result = array[i];
+        if ((result < array[i] && isMax == true)
+         || (result > array[i] && isMax == false))
+        {
+            result = array[i];
+        }
     }
 
     return result;
