e6486bb02eac2055333418fcb0ddc8d94c6bed27
Eugene Menski <eugene.menski@gmail.com>
2022-05-31 18:23:28 +0300

    Get element by its position

--- task50/Program.cs
+++ task50/Program.cs
@@ -6,3 +6,74 @@
 
 17 -> такого числа в массиве нет
 5 -> 9 */
+
+int [,] generateArray(int rowsNumber, int columnsNumber, int scale)
+{
+    int [,] array = new int[rowsNumber, columnsNumber];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, scale);
+        }
+    }
+
+    return array;
+}
+
+void printArray(int [,] array)
+{
+    Console.Write($"\t\t");
+
+    for (int headerColumnNumber = 0; headerColumnNumber < array.GetLength(1); headerColumnNumber++)
+    {
+        Console.Write($"[{headerColumnNumber}]\t");
+    }
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"\nСтрока {i}\t");
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}\t");
+        }
+    }
+}
+
+void getElementByPosition(int [,] array, int searchedPosition)
+{
+    if (searchedPosition > (array.GetLength(0) * array.GetLength(1) - 1))
+    {
+        Console.WriteLine("Такого элемента нет!");
+    }
+    else
+    {
+        int currentPosition = 0;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (currentPosition == searchedPosition)
+                {
+                    Console.WriteLine($"Элемент, находящийся на позиции {currentPosition} равен: {array[i, j]}");
+                }
+                currentPosition++;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Сгенерированный массив случайных чисел:");
+
+int [,] randomArray = generateArray(5, 5, 10);
+
+printArray(randomArray);
+
+Console.Write("\nВведите позицию элемента: ");
+
+int position = Convert.ToInt32(Console.ReadLine());
+
+getElementByPosition(randomArray, position);
